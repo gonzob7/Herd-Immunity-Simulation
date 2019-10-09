466d95e0a261676276427655997c065e51f51ce9
Gonzalo Birrueta <silentcr7@gmail.com>
2019-10-08 17:08:20 -0700

    finished write_metadata and log_interaction in logger.py

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ class Logger(object):
     def __init__(self, file_name):
         # TODO:  Finish this initialization method. The file_name passed should be the
         # full file name of the file that the logs will be written to.
-        self.file_name = None
+        self.file_name = file_name
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
@@ -23,7 +23,10 @@ class Logger(object):
         # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+
+        file = open(self.file_name, 'w')
+        file.write(f"pop_size: {pop_size} \t vacc_percentage: {vacc_percentage} \t virus: {virus_name} \t mortality_rate: {mortality_rate} \t basic_repro_num: {basic_repro_num}\n")
+        file.close()
 
     def log_interaction(self, person, random_person, random_person_sick=None,
                         random_person_vacc=None, did_infect=None):
@@ -40,8 +43,19 @@ class Logger(object):
         # represent all the possible edge cases. Use the values passed along with each person,
         # along with whether they are sick or vaccinated when they interact to determine
         # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
 
+        file = open(self.file_name, "a")
+        if did_infect == True:
+            file.write(f"Person {person._id} infected {random_person._id}\n"
+        elif random_person_sick == True:
+            file.write(f"Person {person._id} cannot infect person {random_person._id} because they're already infected\n")
+        elif random_person_vacc == True:
+            file.write(f"Person {person._id} cannot infect person {random_person._id} because they're vaccinated\n")
+        else:
+            file.write(f"Person {person._id} cannot infect person {random_person._id}\n")
+        file.close()
+
+        
     def log_infection_survival(self, person, did_die_from_infection):
         ''' The Simulation object uses this method to log the results of every
         call of a Person object's .resolve_infection() method.
